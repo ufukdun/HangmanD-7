153172f2a4148ba858d6b6471a72181b5eaefce2
ufuk <ufuk@UFUKDUNDAR>
2016-12-24 21:54:35 +0300

    Add project files.

---
+++ HangmanD-7/Hangman.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace HangmanD_7
+{
+    partial class Hangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hangmanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWordLen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGuessesLeft = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWrongguesses = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hangmanToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1731, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hangmanToolStripMenuItem
+            // 
+            this.hangmanToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.hangmanToolStripMenuItem.Name = "hangmanToolStripMenuItem";
+            this.hangmanToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.hangmanToolStripMenuItem.Text = "Hangman";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(411, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(796, 62);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(435, 235);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(615, 176);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1215, 32);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 378);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(407, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Length";
+            // 
+            // txtWordLen
+            // 
+            this.txtWordLen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWordLen.Location = new System.Drawing.Point(481, 109);
+            this.txtWordLen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtWordLen.Name = "txtWordLen";
+            this.txtWordLen.ReadOnly = true;
+            this.txtWordLen.Size = new System.Drawing.Size(53, 26);
+            this.txtWordLen.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(542, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Guess Left";
+            // 
+            // txtGuessesLeft
+            // 
+            this.txtGuessesLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuessesLeft.Location = new System.Drawing.Point(652, 106);
+            this.txtGuessesLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtGuessesLeft.Name = "txtGuessesLeft";
+            this.txtGuessesLeft.ReadOnly = true;
+            this.txtGuessesLeft.Size = new System.Drawing.Size(51, 26);
+            this.txtGuessesLeft.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(720, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Wrong Guess";
+            // 
+            // txtWrongguesses
+            // 
+            this.txtWrongguesses.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWrongguesses.Location = new System.Drawing.Point(851, 106);
+            this.txtWrongguesses.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtWrongguesses.Name = "txtWrongguesses";
+            this.txtWrongguesses.ReadOnly = true;
+            this.txtWrongguesses.Size = new System.Drawing.Size(169, 24);
+            this.txtWrongguesses.TabIndex = 5;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(192, 177);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 31);
+            this.lblInfo.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(435, 418);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(896, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1731, 654);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtWrongguesses);
+            this.Controls.Add(this.txtGuessesLeft);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWordLen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Hangman";
+            this.Text = "Hangman";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hangmanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWordLen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGuessesLeft;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWrongguesses;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ HangmanD-7/Hangman.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HangmanD_7
+{
+    public partial class Hangman : Form
+    {
+        enum HangState
+        {
+            None,
+            Piller,
+            Rope,
+            Head,
+            body,
+            LeftHand,
+            RightHand,
+            LeftLeg,
+            RightLeg
+        }
+
+        // Holds currnent word characters
+        List<Label> labels = new List<Label>();
+        // Word under consideration
+        public string currentWord { get; set; }
+        // Default character for hidden word letters
+        public string DefaultChar { get { return "__"; } }
+        // Current hangstate, used specially to repaint panel grphics
+        private HangState CurrentHangState = HangState.None;
+        // HangState enum size
+        public int HangStateSize { get { return (Enum.GetValues(typeof(HangState)).Length - 1); } }
+        
+        // Global graphics data
+        Pen p;
+        Pen pRope;
+        int panelLocX = 0;
+        int panelLocY = 0;
+        int panelWidth = 0;
+        int panelHeight = 0;
+
+        // Piller endpoints (We are not storing in Point object since we need individual x, y-coordinate)
+        int pillerVerBottomX;
+        int pillerVerBootomY;
+        int pillerVerTopX;
+        int pillerVerTopY;
+        int pillerHorRightX;
+        int pillerHorRightY;
+        int pillerHorLeftX;
+        int pillerHorLeftY;
+
+        // Rope data
+        int ropeTopX;
+        int ropeTopY;
+        int ropeBottomX;
+        int ropeBottomY;
+
+        // Head Data
+        int diameter = 40;
+        int HeadBoundingRectX;
+
+        // Body data
+        int bodyBoundingRectY;
+        int bodySize;
+
+        public Hangman()
+        {
+            InitializeComponent();
+            AddButtons();
+        }
+
+        /// <summary>
+        /// Adds buttons
+        /// </summary>
+        private void AddButtons()
+        {
+            for (int i = (int)'A'; i <= (int)'Z'; i++)
+            {
+                Button b = new Button();
+                b.Text = ((char)i).ToString();
+                b.Parent = flowLayoutPanel1;
+                b.Font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold);
+                b.Size = new Size(40, 40);
+                b.BackColor = Color.LawnGreen;                
+                b.Click += b_Click; // Event hook-up
+            }
+
+            // Disabling buttons
+            flowLayoutPanel1.Enabled = false;
+        }
+
+        void b_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            char charClicked = b.Text.ToCharArray()[0];
+            b.Enabled = false;
+
+            if ((currentWord = currentWord.ToUpper()).Contains(charClicked))
+            {
+                // char is there (right guess)
+                lblInfo.Text = "Awesome!";
+                lblInfo.ForeColor = Color.Green;
+                char[] charArray = currentWord.ToCharArray();
+                for (int i = 0; i < currentWord.Length; i++)
+                {
+                    if (charArray[i] == charClicked)
+                        labels[i].Text = charClicked.ToString();
+                }
+
+                // Winning condition               
+                if (labels.Where(x => x.Text.Equals(DefaultChar)).Any())
+                    return;
+
+                lblInfo.ForeColor = Color.Green;
+                lblInfo.Text = "Hurray! You win.";
+                flowLayoutPanel1.Enabled = false;
+            }
+            else
+            {
+                // Wrong guess
+                lblInfo.Text = "Boo..";
+                lblInfo.ForeColor = Color.Brown;
+                if (CurrentHangState != HangState.RightLeg)
+                    CurrentHangState++;
+                txtGuessesLeft.Text = (HangStateSize - (int)CurrentHangState).ToString();
+                txtWrongguesses.Text += string.IsNullOrWhiteSpace(txtWrongguesses.Text) ? charClicked.ToString() : "," + charClicked;
+
+                panel1.Invalidate();
+
+                if (CurrentHangState == HangState.RightLeg)
+                {
+                    lblInfo.Text = "You lose!";
+                    lblInfo.ForeColor = Color.Red;
+                    flowLayoutPanel1.Enabled = false;
+
+                    // Reveal the word
+                    for (int i = 0; i < currentWord.Length; i++)
+                    {
+                        if (labels[i].Text.Equals(DefaultChar))
+                        {
+                            labels[i].Text = currentWord[i].ToString();
+                            labels[i].ForeColor = Color.Blue;
+                        }
+                    }
+                }
+            }
+        }        
+
+        private void InitializeVars()
+        {
+            // Global graphics data            
+            p = new Pen(Color.Blue, 20);
+            pRope = new Pen(Color.Blue, 5);
+            panelLocX = panel1.Location.X;
+            panelLocY = panel1.Location.Y;
+            panelWidth = panel1.Width;
+            panelHeight = panel1.Height;
+
+            // Piller endpoints (We are not storing in Point member since we need individual x, y-coordinate)
+            pillerVerBottomX = panelWidth - 30;
+            pillerVerBootomY = panelHeight - 15;
+            pillerVerTopX = pillerVerBottomX;
+            pillerVerTopY = panelHeight - panelHeight + 30;
+            pillerHorRightX = panelWidth - 30 + 10;
+            pillerHorRightY = panelHeight - panelHeight + 50;
+            pillerHorLeftX = panelWidth - panelWidth + 50;
+            pillerHorLeftY = pillerHorRightY;
+
+            // Rope data
+            ropeTopX = (pillerHorRightX + pillerHorLeftX) / 3;
+            ropeTopY = pillerHorLeftY;
+            ropeBottomX = ropeTopX;
+            ropeBottomY = ropeTopY + 30; // 30 is rope length
+
+            // Head Data
+            diameter = 40;
+            HeadBoundingRectX = ropeBottomX - diameter / 2;
+
+            // Body data
+            bodyBoundingRectY = ropeBottomY + diameter;
+            bodySize = (pillerVerBootomY - pillerVerTopY) / 2;
+        }        
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (flowLayoutPanel1.Enabled)
+                if (MessageBox.Show("Game in progress, wanna start again?", "Game in progress", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.Cancel)
+                    return;
+
+            ResetControls();
+            SelectWord();
+            AddLabels();
+        }
+
+        private void AddLabels()
+        {
+            // Adding word to labels and labels to group Box
+            groupBox1.Controls.Clear();
+            labels.Clear();
+            char[] wordChars = currentWord.ToCharArray();
+            int len = wordChars.Length;
+            int refer = groupBox1.Width / len;
+
+            for (int i = 0; i < len; i++)
+            {
+                Label l = new Label();
+                l.Text = DefaultChar;
+                l.Location = new Point(10 + i * refer, groupBox1.Height - 30);
+                l.Parent = groupBox1;
+                l.BringToFront();
+                labels.Add(l);
+            }
+
+            // Writting text boxes 
+            txtWordLen.Text = len.ToString();
+            txtGuessesLeft.Text = HangStateSize.ToString();
+        }
+
+        private void ResetControls()
+        {
+            // Resetting things
+            flowLayoutPanel1.Controls.Clear();
+            AddButtons();
+            CurrentHangState = HangState.None;
+            panel1.Invalidate();
+            txtWrongguesses.Clear();
+            lblInfo.Text = "";
+            flowLayoutPanel1.Enabled = true;
+        }
+
+        /// <summary>
+        /// Randomizes a word reading text file (Words.txt) from current directory (exe location)
+        /// </summary>
+        private void SelectWord()
+        {                      
+            string filePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Words.txt");
+            using (TextReader tr = new StreamReader(filePath, Encoding.ASCII))
+            {
+                Random r = new Random();
+                var allWords = tr.ReadToEnd().Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);                
+                currentWord = allWords[r.Next(0, allWords.Length - 1)];
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            InitializeVars();
+            var g = e.Graphics; //Graphic to draw on
+
+            if (CurrentHangState >= HangState.Piller)
+            {
+                g.DrawLine(p, new Point(pillerVerBottomX, pillerVerBootomY), new Point(pillerVerTopX, pillerVerTopY));
+                g.DrawLine(p, new Point(pillerHorRightX, pillerHorRightY), new Point(pillerHorLeftX, pillerHorLeftY));
+            }
+
+            if (CurrentHangState >= HangState.Rope)
+            {
+                g.DrawLine(pRope, new Point(ropeTopX, ropeTopY), new Point(ropeBottomX, ropeBottomY));
+            }
+
+            if (CurrentHangState >= HangState.Head)
+            {
+                g.DrawEllipse(pRope, new Rectangle(new Point(HeadBoundingRectX, ropeBottomY), new Size(diameter, diameter)));
+                g.FillRectangles(new SolidBrush(Color.Crimson),
+                    new[] {
+                            new Rectangle( new Point(HeadBoundingRectX + 10, ropeBottomY + 10), new Size(6, 6)), // Left eye
+                            new Rectangle( new Point(HeadBoundingRectX + diameter - 10 - 6, ropeBottomY + 10), new Size(6, 6)), // Right eye
+                            new Rectangle(new Point(ropeBottomX - 5/2, ropeBottomY + diameter/2), new Size(5, 5)),  // Nose
+                            new Rectangle(new Point(ropeBottomX - 10, ropeBottomY + diameter/2 + 10), new Size(20, 5))   // Mouth
+                        });
+            }
+
+            if (CurrentHangState >= HangState.body)
+            {
+                g.DrawEllipse(pRope, new Rectangle(new Point(HeadBoundingRectX, bodyBoundingRectY), new Size(diameter, bodySize)));
+            }
+
+            if (CurrentHangState >= HangState.LeftHand)
+            {
+                g.DrawCurve(pRope,
+                new[] { 
+                            new Point(HeadBoundingRectX + 8, bodyBoundingRectY + 15), 
+                            new Point(HeadBoundingRectX - 30, bodyBoundingRectY + 30),
+                            new Point(HeadBoundingRectX - 30, bodyBoundingRectY + 20),
+                            
+                            new Point(HeadBoundingRectX + 5, bodyBoundingRectY + 25)
+                        });
+            }
+
+            if (CurrentHangState >= HangState.RightHand)
+            {
+                g.DrawCurve(pRope,
+                 new[] { 
+                            new Point(HeadBoundingRectX + diameter - 8, bodyBoundingRectY + 15), 
+                            new Point(HeadBoundingRectX + diameter + 30, bodyBoundingRectY + 30),
+                            new Point(HeadBoundingRectX + diameter + 30, bodyBoundingRectY + 20),
+                            
+                            new Point(HeadBoundingRectX + diameter - 5, bodyBoundingRectY + 25)
+                        });
+            }
+
+            if (CurrentHangState >= HangState.LeftLeg)
+            {
+                g.DrawCurve(pRope,
+                new[] { 
+                            new Point(HeadBoundingRectX + 8, bodyBoundingRectY + bodySize - 15), 
+                            new Point(HeadBoundingRectX - 30, bodyBoundingRectY + bodySize - 5),
+                            new Point(HeadBoundingRectX - 30, bodyBoundingRectY + bodySize),
+                            new Point(HeadBoundingRectX + 5, bodyBoundingRectY + bodySize - 25)
+                        });
+            }
+
+            if (CurrentHangState >= HangState.RightLeg)
+            {
+                g.DrawCurve(pRope,
+                  new[] { 
+                            new Point(HeadBoundingRectX + diameter - 8, bodyBoundingRectY + bodySize - 15), 
+                            new Point(HeadBoundingRectX + diameter + 30, bodyBoundingRectY + bodySize - 5),
+                            new Point(HeadBoundingRectX + diameter + 30, bodyBoundingRectY + bodySize),
+                            new Point(HeadBoundingRectX + diameter - 5, bodyBoundingRectY + bodySize - 25)
+                        });
+            }
+        }       
+    }
+}
